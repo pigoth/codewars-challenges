dc6bcc2993cbdbeaec015115a31f0b8a3ead0b57
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-01-15 18:21:49 +0100

    add CollatzConjectureCounter

---
+++ src/main/java/org/example/challenge/cracking/digital/hash/CollatzConjectureCounter.java
@@ -0,0 +1,32 @@
+package org.example.challenge.cracking.digital.hash;
+
+import java.math.BigInteger;
+
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.valueOf;
+
+public class CollatzConjectureCounter {
+
+    public static long countCollatzNumbers(BigInteger number) {
+        long counter = 0;
+
+        while (isGreaterThanOne(number)) {
+            boolean isOdd = number.testBit(0);
+            number = isOdd ? oddCase(number) : evenCase(number);
+            counter += 1;
+        }
+        return counter;
+    }
+
+    private static BigInteger evenCase(BigInteger number) {
+        return number.divide(valueOf(2));
+    }
+
+    private static BigInteger oddCase(BigInteger number) {
+        return number.multiply(valueOf(3)).add(valueOf(1));
+    }
+
+    private static boolean isGreaterThanOne(BigInteger number) {
+        return number.compareTo(ONE) == 1;
+    }
+}
---
+++ src/test/java/org/example/challenge/cracking/digital/hash/CollatzConjectureCounterTest.java
@@ -0,0 +1,25 @@
+package org.example.challenge.cracking.digital.hash;
+
+import org.junit.jupiter.api.Test;
+
+import static java.math.BigInteger.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.challenge.cracking.digital.hash.CollatzConjectureCounter.countCollatzNumbers;
+
+class CollatzConjectureCounterTest {
+    @Test
+    void should_return_0_when_input_is_one() {
+        assertThat(countCollatzNumbers(valueOf(1))).isEqualTo(0L);
+    }
+
+    @Test
+    void should_divide_by_two_when_even_and_count_the_iteration() {
+        assertThat(countCollatzNumbers(valueOf(2))).isEqualTo(1L);
+    }
+
+    @Test
+    void should__when_even_and_count_the_iteration() {
+        assertThat(countCollatzNumbers(valueOf(3))).isEqualTo(7L);
+    }
+
+}
