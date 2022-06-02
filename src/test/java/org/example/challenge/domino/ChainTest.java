811e0986d3584ad8d1273930ef4a694145d37cfd
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-06-02 16:22:15 +0200

    test more complex chains

--- src/test/java/org/example/challenge/domino/ChainTest.java
+++ src/test/java/org/example/challenge/domino/ChainTest.java
@@ -4,38 +4,56 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChainTest {
 
     @Test
     void should_create_all_chain_with_min_size_as_two() {
-        List<Tile> tiles = List.of(new Tile(3, 4), new Tile(4, 6));
+        List<Tile> tiles = of(new Tile(3, 4), new Tile(4, 6));
 
         List<List<Tile>> result = new Chains(tiles).all(it -> true);
 
         assertThat(result).containsExactlyInAnyOrder(
-                List.of(new Tile(3, 4), new Tile(4, 6)),
-                List.of(new Tile(6, 4), new Tile(4, 3))
+                of(new Tile(3, 4), new Tile(4, 6)),
+                of(new Tile(6, 4), new Tile(4, 3))
         );
     }
 
     @Test
     void should_create_all_chain_with_match_in_same_position() {
-        List<Tile> tiles = List.of(new Tile(3, 4), new Tile( 6, 4));
+        List<Tile> tiles = of(new Tile(3, 4), new Tile(6, 4));
 
         List<List<Tile>> result = new Chains(tiles).all(it -> true);
 
         assertThat(result).containsExactlyInAnyOrder(
-                List.of(new Tile(3, 4), new Tile(4, 6)),
-                List.of(new Tile(6, 4), new Tile(4, 3))
+                of(new Tile(3, 4), new Tile(4, 6)),
+                of(new Tile(6, 4), new Tile(4, 3))
+        );
+    }
+
+
+    @Test
+    void should_create_all_chain_with_almost_min_size() {
+        List<Tile> tiles = of(new Tile(3, 4), new Tile(6, 4), new Tile(1, 6));
+
+        List<List<Tile>> result = new Chains(tiles).all(it -> true);
+
+        assertThat(result).containsExactlyInAnyOrder(
+                of(new Tile(3, 4), new Tile(4, 6)),
+                of(new Tile(3, 4), new Tile(4, 6), new Tile(6, 1)),
+                of(new Tile(6, 4), new Tile(4, 3)),
+                of(new Tile(4, 6), new Tile(6, 1)),
+                of(new Tile(1, 6), new Tile(6, 4)),
+                of(new Tile(1, 6), new Tile(6, 4), new Tile(4, 3))
         );
     }
 
     @Test
     void should_ignore_tiles_with_no_matching_values() {
         Tile noMatchingValues = new Tile(1, 5);
-        List<Tile> tiles = List.of(new Tile(3, 4), noMatchingValues, new Tile( 6, 4));
+        List<Tile> tiles = of(new Tile(3, 4), noMatchingValues, new Tile(6, 4));
 
         List<List<Tile>> result = new Chains(tiles).all(it -> true);
 
