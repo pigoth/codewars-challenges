082d42afcd67f7ce858e953e46b2691cc3a5bb1f
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-08-22 14:09:10 +0200

    refactor : improve code reading

--- src/main/java/org/example/challenge/string/incrementer/Kata.java
+++ src/main/java/org/example/challenge/string/incrementer/Kata.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.Math.max;
 import static java.lang.String.valueOf;
 import static java.math.BigInteger.ONE;
 
@@ -20,6 +21,6 @@ public class Kata {
     private static String increment(String value) {
         var result = new BigInteger(value).add(ONE);
         int paddingSize = value.length() - valueOf(result).length();
-        return "0".repeat(paddingSize > 0 ? paddingSize : 0) + result;
+        return "0".repeat(max(paddingSize, 0)) + result;
     }
 }
